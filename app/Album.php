13548caeafa66d09ffb19ade3817c912ccff7c2f
Asif Ali <shea786@live.co.uk>
2017-07-30 22:48:20 +0100

    updated relationships in Album model

--- app/Album.php
+++ app/Album.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Album extends Model
 {
-    //
+    public function Reciters()
+    {
+        return $this->belongsTo('App\Reciter', 'reciters_id', 'id');
+    }
+
+    public function Tracks()
+    {
+        return $this->hasMany('App\Track', 'albums_id', 'id');
+    }
 }
