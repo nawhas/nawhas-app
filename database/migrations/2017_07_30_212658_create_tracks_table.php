29621efeea90e8e33c1da3d14df0e6b507cbdc9e
vagrant <vagrant@homestead>
2017-07-30 21:28:29 +0000

    added Track model and its migration

---
+++ database/migrations/2017_07_30_212658_create_tracks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('albums_id');
+            $table->string('file_path');
+            $table->integer('hits');
+            $table->integer('track_number');
+            $table->smallInteger('status')->default(0);
+            $table->dateTime('moderated_at')->nullable();
+            $table->integer('moderated_by')->nullable()->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}
