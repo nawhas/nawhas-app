ac8918a0896afccce32d51e1144ec3efb41a922f
vagrant <vagrant@homestead>
2017-07-30 21:21:04 +0000

    added Album model and its migration

---
+++ database/migrations/2017_07_30_211847_create_albums_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('reciters_id');
+            $table->integer('year');
+            $table->integer('hits');
+            $table->string('image_path');
+            $table->smallInteger('status')->default(0);
+            $table->dateTime('moderated_at')->nullable();
+            $table->integer('moderated_by')->nullable()->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
