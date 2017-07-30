8a886c441b9481c24fb4cc9582f6858bf04fbc44
vagrant <vagrant@homestead>
2017-07-30 21:46:37 +0000

    added relationship to Reciter model

--- app/Reciter.php
+++ app/Reciter.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reciter extends Model
 {
-    //
+    public function Albums()
+    {
+        return $this->hasMany('App\Album', 'reciters_id', 'id');
+    }
 }
