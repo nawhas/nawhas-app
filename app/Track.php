0b2d78f8551d9c9c68572af5052c8fc9c44acc0d
Asif Ali <shea786@live.co.uk>
2017-07-30 22:50:51 +0100

    updated relationships in Track model

--- app/Track.php
+++ app/Track.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Track extends Model
 {
-    //
+    public function Albums()
+    {
+        return $this->belongsTo('App\Album', 'albums_id', 'id');
+    }
+
+    public function Lyrics()
+    {
+        return $this->hasOne('App\Lyric', 'tracks_id', 'id');
+    }
 }
