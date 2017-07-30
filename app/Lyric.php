557b3f046d954886b063a9f281a9f959a90f9497
Asif Ali <shea786@live.co.uk>
2017-07-30 22:49:41 +0100

    updated relationships in Lyric model

--- app/Lyric.php
+++ app/Lyric.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lyric extends Model
 {
-    //
+    public function Tracks()
+    {
+        return $this->belongsTo('App\Track', 'tracks_id', 'id');
+    }
 }
