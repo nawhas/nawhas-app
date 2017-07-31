1f1ab07028b32048fb253c56fe4360d1a78119d7
vagrant <vagrant@homestead>
2017-07-31 18:32:53 +0000

    created reciters controller as well as created the get /reciters api route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')->prefix('reciters')->group(function () {
+    Route::get('/', 'RecitersController@index');
+});
