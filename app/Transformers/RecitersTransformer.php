a33374e7c94dd221d7a43615adc28b1e682f7064
Asif Ali <shea786@live.co.uk>
2017-08-14 20:20:47 +0000

    changed date formatting and adding first, middle and last names instead of full name

--- app/Transformers/RecitersTransformer.php
+++ app/Transformers/RecitersTransformer.php
@@ -7,11 +7,33 @@ use League\Fractal\TransformerAbstract;
 
 class RecitersTransformer extends TransformerAbstract
 {
+    /**
+     * @param Reciter $reciter
+     * @return array
+     */
     public function transform(Reciter $reciter)
     {
+        $name = explode(" ", $reciter->name);
+        $first_name = '';
+        $middle_name = '';
+        $last_name = '';
+        $i = 0;
+        $len = count($name);
+        foreach ($name as $item) {
+            if ($i == 0) {
+                $first_name .= $item;
+            } else if ($i == $len - 1) {
+                $last_name .= $item;
+            } else {
+                $middle_name .= $item . ' ';
+            }
+            $i++;
+        }
         return [
             'id' => $reciter->id,
-            'name' => $reciter->name,
+            'first_name' => $first_name,
+            'middle_name' => $middle_name,
+            'last_name' => $last_name,
             'image' => $reciter->image_path,
             'created_at' => $reciter->created_at->format('l jS \\of F Y h:i:s A'),
         ];
