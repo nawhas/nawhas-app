77f9b4058979ef7f40c2cfd3dbe6ef3150f8f6f3
Zain Mehdi <szainmehdi@gmail.com>
2017-08-14 22:40:33 -0700

    Set up env and redirects.

---
+++ app/Http/Controllers/Auth/RedirectsToFrontend.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use Request;
+
+trait RedirectsToFrontend
+{
+    protected function redirectTo()
+    {
+        $redirect = Request::get('redirect');
+
+        // TODO - Secure redirects by checking hostname
+
+        if (!$redirect) {
+            return env('FRONTEND_URL') . '/auth/redirect';
+        }
+
+        return $redirect;
+    }
+}
