12d01e1541225f39e0ea41f65cf786ac9bf07d5c
Asif Ali <shea786@live.co.uk>
2017-08-18 23:00:38 +0000

    added search use searchable to album, lyric and track models

--- app/Lyric.php
+++ app/Lyric.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class Lyric extends Model
 {
+    use Searchable;
+
     public function tracks()
     {
         return $this->belongsTo(Track::class, 'track_id', 'id');
--- app/Track.php
+++ app/Track.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class Track extends Model
 {
+    use Searchable;
+
     public function albums()
     {
         return $this->belongsTo(Album::class, 'album_id', 'id');
