b09fc5d71f28fb7f89598af654943853703c9fe0
Asif Ali <shea786@live.co.uk>
2017-09-11 22:48:35 +0000

    updated to meet styling standard

--- app/Console/Commands/Data/ImportDataCommand.php
+++ app/Console/Commands/Data/ImportDataCommand.php
@@ -46,8 +46,9 @@ class ImportDataCommand extends Command
 
         $this->comment('Verifying installation prerequisites...');
         $dir = 'public/uploads/reciters';
-        if(!File::exists($dir)) {
+        if (!File::exists($dir)) {
             $this->error('Please create reciters folder');
+
             return;
         } else {
             $this->comment('The reciters folder exists');
@@ -58,8 +59,9 @@ class ImportDataCommand extends Command
         unset($reciterContent[0]);
         unset($reciterContent[1]);
         $reciterContent = array_values($reciterContent);
-        if(!count($reciterContent) > 0) {
-            $this->error("There are no reciters");
+        if (!count($reciterContent) > 0) {
+            $this->error('There are no reciters');
+
             return;
         } else {
             $this->comment('reciters were found');
@@ -74,29 +76,32 @@ class ImportDataCommand extends Command
             $reciterAvaterJPEG = $reciterDir . '/avatar.jpg';
 
             // check if the reciter does not exist in db
-            if(!$reciter = Reciter::where('slug', $reciterSlug)->first()) {
+            if (!$reciter = Reciter::where('slug', $reciterSlug)->first()) {
                 // check if bio file exists
-                if(!file_exists($reciterBio)) {
+                if (!file_exists($reciterBio)) {
                     $this->error('bio.txt does not exist for ' . $reciterName);
+
                     return;
                 } else {
                     $this->comment('successfully found bio.txt');
                 }
-                if(!$reciterBio = file_get_contents($reciterBio, true)) {
+                if (!$reciterBio = file_get_contents($reciterBio, true)) {
                     $this->error('An error occurred when trying to read bio.txt for ' . $reciterName);
+
                     return;
                 } else {
                     $this->comment('Successfully imported bio from txt');
                 }
 
                 // check if avatar exists
-                if(file_exists($reciterAvaterJPEG)) {
+                if (file_exists($reciterAvaterJPEG)) {
                     $reciterAvatarFile = 'avatar.jpg';
-                } else if(file_exists($reciterAvaterPNG)) {
+                } elseif (file_exists($reciterAvaterPNG)) {
                     $reciterAvatarFile = 'avatar.png';
                 }
-                if(!$reciterAvatarFile) {
+                if (!$reciterAvatarFile) {
                     $this->error($reciterName . ' does not have an avatar.png or avatar.jpg file');
+
                     return;
                 } else {
                     $this->comment('Avatar found for ' . $reciterName);
@@ -109,8 +114,9 @@ class ImportDataCommand extends Command
                 $reciter->description = $reciterBio;
                 $reciter->image_path = $reciterAvatarFile;
                 $reciter->created_by = 1;
-                if(!$reciter->save()) {
+                if (!$reciter->save()) {
                     $this->error('Reciter did not save');
+
                     return;
                 } else {
                     $this->comment('Reciter ' . $reciterName . ' has been created');
@@ -127,8 +133,8 @@ class ImportDataCommand extends Command
             unset($albumsContent[0]);
             unset($albumsContent[1]);
             $albumsContent = array_values($albumsContent);
-            if(!count($albumsContent) > 0) {
-                $this->error("There are no albums for " . $reciterName);
+            if (!count($albumsContent) > 0) {
+                $this->error('There are no albums for ' . $reciterName);
             } else {
                 $this->comment('albums were found');
             }
@@ -140,21 +146,22 @@ class ImportDataCommand extends Command
 
                 //check if album exists
                 $albumDir = $albumsDir . '/' . $albumContent;
-                if(!$album = $reciter->albums->where('year', $albumYear)->first()) {
+                if (!$album = $reciter->albums->where('year', $albumYear)->first()) {
                     $albumArtworkFile = '';
                     $albumArtworkPNG = $albumDir . '/artwork.png';
                     $albumArtworkJPEG = $albumDir . '/artwork.jpg';
                     //check if artwork exists
-                    if(file_exists($albumArtworkPNG)) {
+                    if (file_exists($albumArtworkPNG)) {
                         $albumArtworkFile = 'artwork.jpg';
-                    } else if(file_exists($albumArtworkJPEG)) {
+                    } elseif (file_exists($albumArtworkJPEG)) {
                         $albumArtworkFile = 'artwork.png';
                     }
-                    if(!$albumArtworkFile) {
+                    if (!$albumArtworkFile) {
                         $this->error($reciterName . ' does not have an artowrk.png or artwork.jpg for year' . $albumYear);
+
                         return;
                     } else {
-                        $this->comment('Avatar found for ' . $reciterName . ' Album '. $albumYear);
+                        $this->comment('Avatar found for ' . $reciterName . ' Album ' . $albumYear);
                     }
                     // create Album
                     $album = new Album;
@@ -165,7 +172,7 @@ class ImportDataCommand extends Command
                     $album->created_by = 1;
                     $album->save();
                 } else {
-                    $this->comment($albumYear .' Album Found');
+                    $this->comment($albumYear . ' Album Found');
                 }
 
                 // tracks section
@@ -176,8 +183,8 @@ class ImportDataCommand extends Command
                 unset($tracksContent[0]);
                 unset($tracksContent[1]);
                 $tracksContent = array_values($tracksContent);
-                if(!count($tracksContent) > 0) {
-                    $this->error("There are no tracks for album " . $albumYear . ' for reciter ' . $reciterName);
+                if (!count($tracksContent) > 0) {
+                    $this->error('There are no tracks for album ' . $albumYear . ' for reciter ' . $reciterName);
                 } else {
                     $this->comment('tracks were found');
                 }
@@ -185,9 +192,10 @@ class ImportDataCommand extends Command
                     list($trackNumber, $trackName) = explode(' - ', $trackContent);
                     $trackDir = $tracksDir . '/' . $trackContent;
                     $trackSlug = str_slug($trackName);
-                    if(!$track = Track::where('slug', $trackSlug)->first()){
-                        if(!file_exists($trackDir . '/audio.mp3')) {
+                    if (!$track = Track::where('slug', $trackSlug)->first()) {
+                        if (!file_exists($trackDir . '/audio.mp3')) {
                             $this->error('no audio file');
+
                             return;
                         }
                         $track = new Track();
@@ -206,13 +214,14 @@ class ImportDataCommand extends Command
 
                     // lyrics
                     $this->comment('checking for prerequisites for lyrics');
-                    if(!$lyric = $track->lyrics) {
-                        if(!file_exists($trackDir . '/lyrics.txt')) {
+                    if (!$lyric = $track->lyrics) {
+                        if (!file_exists($trackDir . '/lyrics.txt')) {
                             $this->error('Lyric does not exist for reciter ' . $reciterName . ' Album ' . $albumYear . ' Track ' . $trackName);
                         } else {
                             $lyricFile = $trackDir . '/lyrics.txt';
-                            if(!$lyricFile = file_get_contents($lyricFile, true)) {
+                            if (!$lyricFile = file_get_contents($lyricFile, true)) {
                                 $this->error('An error occurred when trying to read lyrics.txt for');
+
                                 return;
                             } else {
                                 $this->comment('Successfully imported bio from txt');
@@ -225,7 +234,7 @@ class ImportDataCommand extends Command
                             $lyric->save();
                             $this->comment('Successfully created lyric');
                         }
-                    }else {
+                    } else {
                         $this->comment('Lyric already exists');
                     }
                 }
