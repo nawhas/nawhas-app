cd785c6fde24fbf4a084c5ac5c39717e5579a313
Asif Ali <shea786@live.co.uk>
2017-09-12 22:42:17 +0000

    added popular route

--- routes/api.php
+++ routes/api.php
@@ -54,4 +54,8 @@ Route::namespace('Api')->group(function () {
         Route::patch('/{lyric}', 'LyricsController@update');
         Route::delete('/{lyric}', 'LyricsController@destroy');
     });
+
+    Route::prefix('popular')->group(function () {
+        Route::get('/reciters', 'LyricsController@index');
+    });
 });
