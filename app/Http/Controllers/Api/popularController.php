5b9f0d9626c0cb14dc25f2ad5bc3aaf056e3baa9
Asif Ali <shea786@live.co.uk>
2017-09-14 16:45:01 +0000

    updated popularController

--- app/Http/Controllers/Api/popularController.php
+++ app/Http/Controllers/Api/popularController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Reciter;
+use App\Transformers\ReciterTransformer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class popularController extends Controller
 {
-    static function reciters(Request $request)
+    public function reciters(Request $request, ReciterTransformer $transformer)
     {
+        $this->transformer = $transformer;
         if ($request->get('period')) {
             $period = $request->get('period');
         } else {
             $period = 30;
         }
+
         return Reciter::popularLast($period)->get();
     }
 }
