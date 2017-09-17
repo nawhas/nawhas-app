381f69e6a62a4bb464ccd797f06fdece8ea80696
Asif Ali <shea786@live.co.uk>
2017-09-17 03:38:46 +0000

    added albums and tracks method to Popular controller

--- app/Http/Controllers/Api/PopularEntitiesController.php
+++ app/Http/Controllers/Api/PopularEntitiesController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Album;
 use App\Http\Controllers\TransformsResponses;
 use App\Reciter;
+use App\Track;
+use App\Transformers\AlbumTransformer;
 use App\Transformers\ReciterTransformer;
+use App\Transformers\TrackTransformer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -26,5 +30,25 @@ class PopularEntitiesController extends Controller
         $reciters = Reciter::query()->popularLast($period)->limit($limit)->get();
 
         return $this->respondWithCollection($reciters, $transformer);
+    }
+
+    public function albums(Request $request, AlbumTransformer $transformer)
+    {
+        $period = $request->get('period', 30);
+        $limit = $request->get('limit', 10);
+
+        $albums = Album::query()->popularLast($period)->limit($limit)->get();
+
+        return $this->respondWithCollection($albums, $transformer);
+    }
+
+    public function tracks(Request $request, TrackTransformer $transformer)
+    {
+        $period = $request->get('period', 30);
+        $limit = $request->get('limit', 10);
+
+        $track = Track::query()->popularLast($period)->limit($limit)->get();
+
+        return $this->respondWithCollection($track, $transformer);
     }
 }
