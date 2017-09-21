0e79fac9cd60da706c8c5bd6b66f1e2e6a6329f6
Asif Ali <shea786@live.co.uk>
2017-09-21 11:12:54 +0000

    added commented out seed for model factories for reciter and users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // factory(App\User::class, 50)->create();
+        // factory(App\Reciter::class, 50)->create();
     }
 }
