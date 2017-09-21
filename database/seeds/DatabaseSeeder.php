0d12c84b1aed61bc4499e5cc960c798f41274c4e
Asif Ali <shea786@live.co.uk>
2017-09-21 11:16:41 +0000

    updated databaseSeeder to include all model factories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         // factory(App\User::class, 50)->create();
         // factory(App\Reciter::class, 50)->create();
+        // factory(App\Album::class, 50)->create();
+        // factory(App\Track::class, 50)->create();
+        // factory(App\Lyric::class, 50)->create();
     }
 }
