c0e02db9375e7c4d52458402dddc65a7fecfd700
Asif Ali <shea786@live.co.uk>
2017-10-06 18:28:44 +0000

    updated to match styling standards

--- database/seeds/VoyagerDummyDatabaseSeeder.php
+++ database/seeds/VoyagerDummyDatabaseSeeder.php
@@ -7,7 +7,7 @@ class VoyagerDummyDatabaseSeeder extends Seeder
 {
     use Seedable;
 
-    protected $seedersPath = __DIR__.'/';
+    protected $seedersPath = __DIR__ . '/';
 
     /**
      * Run the database seeds.
