030ac06362f56f75e29528e31f14907522f3567a
Asif Ali <shea786@live.co.uk>
2017-10-06 14:13:55 +0000

    added reciters and albums pages

--- config/voyager.php
+++ config/voyager.php
@@ -167,8 +167,6 @@ return [
 
         'widgets' => [
             'TCG\\Voyager\\Widgets\\UserDimmer',
-            'TCG\\Voyager\\Widgets\\PostDimmer',
-            'TCG\\Voyager\\Widgets\\PageDimmer',
         ],
 
     ],
