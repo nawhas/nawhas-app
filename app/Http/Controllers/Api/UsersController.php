9bbab0e8ff5dfdda306ff972fe6504f6ea0f8df6
Asif Ali <shea786@live.co.uk>
2017-10-15 23:57:38 +0000

    added user api

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,16 +2,26 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Transformers\UsersTransformer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UsersController extends Controller
 {
     /**
+     * UsersController constructor.
+     * @param UsersTransformer $transformer
+     */
+    public function __construct(UsersTransformer $transformer)
+    {
+        $this->middleware('auth:api');
+        $this->transformer = $transformer;
+    }
+    /**
      * @return mixed
      */
     public function show()
     {
-        return Auth::user();
+        return $this->respondWithItem(Auth::user()->id);
     }
 }
